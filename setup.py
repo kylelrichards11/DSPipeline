e948ae4b0573cdb2ab16a2730adbe052a9d11e97
Kyle Richards <kyleLRichards11@gmail.com>
2019-10-29 18:43:00 +0100

    Updated repo name

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A package to create data pipelines",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/kylelrichards11/DS_Pipeline",
+    url="https://github.com/kylelrichards11/DSPipeline",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
