1e340fe5967231fd95d332320ae680c3ff8a8555
Kyle Richards <kyleLRichards11@gmail.com>
2019-10-29 21:18:33 +0100

    Removed uneeded functionality from data managing

--- DSPipeline/data_managing.py
+++ DSPipeline/data_managing.py
@@ -1,30 +1,11 @@
 # External Imports
 import pandas as pd
-import numpy as np
-
-# Internal Imports
-
-################################################################################################
-# DATA MANAGING FUNCTIONS
-################################################################################################
-
-################################################################################################
-# This function combines X and y data into one np array or DataFrame. y_data is always the
-# first column
-def combine_data(X_data, y_data):
-    if type(X_data) == np.ndarray:
-        return np.hstack((y_data, X_data))
-    elif type(X_data) == pd.DataFrame:
-        return pd.concat((y_data, X_data), axis=1)
 
 ################################################################################################
 # Splits data into X and y components
-def split_x_y(data, y_column=0, y_label='label'):
-    if type(data) == np.ndarray:
-        y_data = np.array(data[:, y_column])
-        return np.delete(data, y_column, axis=1), y_data
-    elif type(data) == pd.DataFrame:
+def split_x_y(data, y_label='label'):
+    if type(data) == pd.DataFrame:
         y_data = data[y_label]
         return data.drop(y_label, axis=1), y_data
     else:
-        raise TypeError(__name__ + ".split_x_y takes numpy array or pandas DataFrame, was " + str(type(data)))
+        raise TypeError(f'{__name__}.split_x_y pandas DataFrame, was {str(type(data))}')
